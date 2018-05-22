13d00f896bda3af1c617c26716c675b8a903209a
danfrui <39525006+danfrui@users.noreply.github.com>
2018-05-22 16:29:27 +0200

    Create database.inc.php

---
+++ local/config/database.inc.php
@@ -0,0 +1,15 @@
+<?php
+$conf['dblayer'] = 'mysql';
+$conf['db_base'] = '2.4.1';
+$conf['db_user'] = 'gotcha';
+$conf['db_password'] = 'xxxxxx';
+$conf['db_host'] = 'localhost';
+ 
+$prefixeTable = 'piwigo_';
+ 
+define('PHPWG_INSTALLED', true);
+define('PWG_CHARSET', 'utf-8');
+define('DB_CHARSET', 'utf8');
+define('DB_COLLATE', '');
+ 
+?>
